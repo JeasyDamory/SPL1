7ebf4bb0f41089f6d31fd11a42c6568724ddc67a
JeasyDamory <171wita30@o365.htl-leoben.at>
2018-04-16 16:26:31 +0200

    HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,4 +1,8 @@
 # Hello World mit python
-name = input("What's your name?")
 
+name = input("What's your name?")
 print ("Hello World!", name)
+
+alter = input("How old are you?")
+print("This year you are ", alter)
+print("Next year you are ", int(alter)+1)
